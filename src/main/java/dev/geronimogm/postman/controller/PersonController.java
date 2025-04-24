1ecd88873c4a6c5eaa4ebe78002a54980c3955a6
GeronimoGM <geronimogonzalezmartino@gmail.com>
2025-04-24 19:00:31 -0300

    Delete server servlet context path and add /api prefix in PersonController

--- src/main/java/dev/geronimogm/postman/controller/PersonController.java
+++ src/main/java/dev/geronimogm/postman/controller/PersonController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/persons")
+@RequestMapping("/api/persons")
 @RequiredArgsConstructor
 public class PersonController {
     private final PersonRepository personRepository;
